f151d21f9c8d6d89275cc6f4a2fded20c74f094b
Daniel Hamam <danielhamam@hotmail.com>
2020-03-15 17:08:38 -0400

    Small updates

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -10,6 +10,18 @@
 #include "sfmm.h"
 
 void *sf_malloc(size_t size) {
+
+    // Segregated by size class
+
+    // if size is 0, return NULL
+    if (size == 0) return NULL; // without setting sf_errno
+    else {
+        // Determine overall size, adding header size, padding. (alignment = multiple of 64)
+        size_t size_no_padding = sizeof(sf_header) + sizeof(sf_footer) + size;
+        size_t size_padding = 64 * ( size_no_padding / 64);
+
+    }
+
     return NULL;
 }
 
