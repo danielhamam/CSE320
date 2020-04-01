45426396c5ef6c82761b273ab3b4e0b7a67124c0
Gene Stark <gene@starkeffect.com>
2020-04-01 10:30:56 -0400

    Bring in HW4 basecode from the development repo.

---
+++ hw4/src/master.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+#include "debug.h"
+#include "polya.h"
+
+/*
+ * master
+ * (See polya.h for specification.)
+ */
+int master(int workers) {
+    // TO BE IMPLEMENTED
+    return EXIT_FAILURE;
+}
---
+++ hw4/src/worker.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+#include "debug.h"
+#include "polya.h"
+
+/*
+ * worker
+ * (See polya.h for specification.)
+ */
+int worker(void) {
+    // TO BE IMPLEMENTED
+    return EXIT_FAILURE;
+}
