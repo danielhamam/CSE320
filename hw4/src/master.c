29ba06448c542bf766a0469ff095c7aee749b8eb
Daniel Hamam <danielhamam@hotmail.com>
2020-04-08 23:17:36 -0400

    Updated read/write functions

--- hw4/src/master.c
+++ hw4/src/master.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "debug.h"
 #include "polya.h"
@@ -8,9 +9,32 @@
  * (See polya.h for specification.)
  */
 int master(int workers) {
-    // TO BE IMPLEMENTED
+
+    sf_start();
+
+//                           INITIALIZE THE PROGRAM
+// ------------------------------------------------------------------------
+    pid_t arrayPID[workers];
+    // hold the two pipes for reading and writing
+    int pipefd[2]; // undeclared array
+
+    int count = 0;
+    pid_t tempPID;
+    while (count < workers) {
+        if (pipe(pipefd) == -1) exit(EXIT_FAILURE);
+
+        tempPID = fork();
+
+        if (tempPID == -1) exit(EXIT_FAILURE);
+        else arrayPID[count] = tempPID;
+
+        count++;
+    }
+// ------------------------------------------------------------------------
 
 
 
+
+    sf_end();
     return EXIT_FAILURE;
 }
--- hw4/src/worker.c
+++ hw4/src/worker.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "debug.h"
 #include "polya.h"
@@ -8,18 +9,27 @@
  * (See polya.h for specification.)
  */
 
+// Signal Handlers, don't do anything "compilicated"
 void stop_itself(); // fort worker process to stop itself
+void sighup_handler();
+void sigterm_handler();
+
+
 struct problem *readProblem();
-void writeResult(struct problem *selectedProblem);
+void writeResult(struct result *selectedResult, FILE *out);
 
 int worker(void) {
 
     // TO BE IMPLEMENTED
     // Work on this first and use demo/polya for testing.
 
+    // Worker Process installs handlers for SIGHUP and SIGTERM
+    signal(SIGHUP, sighup_handler);
+    signal(SIGTERM, sigterm_handler);
+
     // continues until find solution, fails to find, or SIGHUP by master to cancel
     // worker reads problems from standard input and writes results to standard output
-    readProblem();
+    struct problem *selectProblem = readProblem();
 
     return EXIT_FAILURE;
 }
@@ -32,25 +42,22 @@ int worker(void) {
 // ------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------
 
-void stop_itself(void) {}
-
 
 /* READING THE PROBLEM FROM INPUT STREAM*/
 // TAKES NOTHING, RETURNS READ PROBLEM
-struct problem *readProblem() {
+struct problem *readProblem(FILE *stream) {
 
     // Initialize new pointer for problem
-    struct problem *read_problem;
-    // read_problem->size = (size_t) 0;
-    // read_problem->type = (short) 0;
+    struct problem *read_problem = NULL;
 
     // Go byte by byte and read the problem
 
     // First, read the size variable
     int count_size = 0;
     int tempSize = 0;
-    while (count_size < sizeof(struct problem)) {
+    while (count_size < sizeof(size_t)) {
         int byte = fgetc(stdin);
+        if (byte == EOF) exit(EXIT_FAILURE);
         tempSize += byte;
         count_size++;
     }
@@ -60,7 +67,8 @@ struct problem *readProblem() {
     int count_type = 0;
     int tempType = 0;
     while(count_type < sizeof(short)) {
-        int byte = fgetc(stdin);
+        int byte = fgetc(stream);
+        if (byte == EOF) exit(EXIT_FAILURE);
         tempType += byte;
         count_type++;
     }
@@ -70,7 +78,8 @@ struct problem *readProblem() {
     int count_ID = 0;
     int tempID = 0;
     while (count_ID < sizeof(short)) {
-        int byte = fgetc(stdin);
+        int byte = fgetc(stream);
+        if (byte == EOF) exit(EXIT_FAILURE);
         tempID += byte;
         count_ID++;
     }
@@ -80,7 +89,8 @@ struct problem *readProblem() {
     int count_nvars = 0;
     int tempNVARS = 0;
     while (count_nvars < sizeof(short)) {
-        int byte = fgetc(stdin);
+        int byte = fgetc(stream);
+        if (byte == EOF) exit(EXIT_FAILURE);
         tempNVARS += byte;
         count_nvars++;
     }
@@ -90,7 +100,8 @@ struct problem *readProblem() {
     int count_var = 0;
     int tempVar = 0;
     while (count_var < sizeof(short)) {
-        int byte = fgetc(stdin);
+        int byte = fgetc(stream);
+        if (byte == EOF) exit(EXIT_FAILURE);
         tempVar += byte;
         count_var++;
     }
@@ -98,10 +109,33 @@ struct problem *readProblem() {
 
     // Sixth, read the char padding[0] variable
     // Seventh, read the char data[0] variable
+    int sizeArrays = (int) read_problem->size - sizeof(struct problem);
 
+    char *array = (char *) malloc(sizeArrays); // We allocated for the amount of padding/data
+    memcpy(read_problem->data, &array, sizeArrays); // Copy from the array we made to the read_problem->data)
 
     return read_problem;
 }
 
 /* WRITING THE PROBLEM TO OUTPUT STREAM*/
-void writeResult(struct problem *selectedProblem) {}
+void writeResult(struct result *selectedResult, FILE *out) {
+
+    char *memHolder = NULL;
+    memcpy(&memHolder, &selectedResult, sizeof(*selectedResult));
+
+    while (memHolder != NULL) {
+        fputc(*memHolder, out);
+        memHolder++;
+    }
+}
+
+void stop_itself(void) {}
+
+void sighup_handler(void) {}
+
+void sigterm_handler(void) {
+
+    // Graceful termination of worker, use exit()
+    exit(2);
+
+}
