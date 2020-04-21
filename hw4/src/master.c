ed7096c4132ac4f5f032b5ee540e458a8c928fae
Daniel Hamam <danielhamam@hotmail.com>
2020-04-21 01:28:11 -0400

    Master updates with pipes

--- hw4/src/master.c
+++ hw4/src/master.c
@@ -13,6 +13,8 @@
 struct problem *generateProblem();
 void writeProblem(struct problem *problem, FILE *stream);
 
+volatile sig_atomic_t *statesWorkers; // to hold workers' states
+
 int master(int workers) {
 
     sf_start();
@@ -20,6 +22,7 @@ int master(int workers) {
 //                           INITIALIZE THE PROGRAM
 // ------------------------------------------------------------------------
 
+    statesWorkers = malloc(sizeof(sig_atomic_t) * workers);
     pid_t arrayPID[workers]; // i dont think you have to malloc because only exist in this code block
 
     int masterToworker_pipes[workers][2]; // ARRAY (PIPE DESCRIPTORS), MASTER SENDS TO WORKER
@@ -31,13 +34,9 @@ int master(int workers) {
         if (pipe(masterToworker_pipes[count]) == -1) exit(EXIT_FAILURE);
         if (pipe(workerTomaster_pipes[count]) == -1) exit(EXIT_FAILURE);
 
+        // CLOSING FILE DESCRIPTORS IN THE PARENT
         // Redirect standard input/output to pipes ( pipe[0] = read end of the pipe, pipe[1] = write end of the pipe )
         // In unistd.h, 0 is file descriptor for standard input, 1 for standard output, 2 standard error output
-        dup2(0, masterToworker_pipes[count][0]); // move pipe into stdin (WRITING END)
-        close(masterToworker_pipes[count][0]); // close READING END of pipe
-
-        dup2(1, workerTomaster_pipes[count][1]); // move pipe into stdout (WRITING END), write worker to masker
-        close(workerTomaster_pipes[count][0]); // close READING END of pipe
 
         count++;
     }
@@ -50,20 +49,30 @@ int master(int workers) {
     while (count < workers) {
 
         pid_t tempPID = fork();
+        arrayPID[count] = tempPID; // When PID == 0, it is child process (are we assuming the worker processes are running? Sent SIGSTOP)
         if (tempPID == -1) exit(EXIT_FAILURE);
         else if (tempPID == 0) {
-            arrayPID[count] = getpid(); // When PID == 0, it is child process (are we assuming the worker processes are running? Sent SIGSTOP)
+
+            debug("Started to work with Worker %d ", getpid());
+
+            sf_change_state(getpid(), 0, WORKER_STARTED); // changing the state to started (0 is init state)
+
+            // change file descriptors in CHILD PROCESS (read = 0, write = 1)
+            close(masterToworker_pipes[count][1]); // child reads from M2W, close write end
+            close(workerTomaster_pipes[count][0]); // child writes to W2M, close read end
+
+            // redirect to standard in/out
+            dup2(masterToworker_pipes[count][1],0); // child reads from M2W, make it stdin (WRITING END)
+            dup2(workerTomaster_pipes[count][0],1); // child writes to W2M, make it stdout (READING END)
 
         } // end of worker process running, would stop with SIGSTOP in worker
+        else {
+            close(masterToworker_pipes[count][0]); // parent writes to M2W, close read end
+            close(workerTomaster_pipes[count][1]); // parent reads from W2M, close write end
+        }
         count++;
     } // end of while loop
 
-    // Wait for each child process to run
-    int count2 = 0;
-    int status;
-    while (count2 < workers) {
-        waitpid(arrayPID[count2], &status, 0);
-    }
 // ------------------------------------------------------------------------
 
 // REPEATEDLY ASSIGNING PROBLEMS TO IDLE WORKERS AND POSTS RESULTS RECEIVED FROM WORKERS ( ithink at this point, all workers should be idle )
